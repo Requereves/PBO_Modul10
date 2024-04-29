7dc98add56050e6c81732fa7b46379fad3df5c3d
Nicholas Musa S S <141598735+Requereves@users.noreply.github.com>
2024-04-29 16:22:35 +0800

    Add files via upload

---
+++ PBOKoneksiModul10/src/pbokoneksimodul10/KoneksiMysql.java
@@ -0,0 +1,39 @@
+package pbokoneksimodul10;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class KoneksiMysql {
+
+    private Connection connect;
+    private String driverName = "com.mysql.cj.jdbc.Driver";
+    private String jdbc = "jdbc:mysql://";
+    private String host = "localhost";
+    private String port = "3306";
+    private String database = "crud2220702359";
+    private String url = jdbc + host + ":" + port + "/" + database;
+    private String username = "nicholas";
+    private String password = "password";
+
+    public Connection getKoneksi() throws SQLException {
+        if (connect == null) {
+            try {
+                Class.forName(driverName);
+                System.out.println("Class Driver Ditemukan");
+
+                try {
+                    connect = DriverManager.getConnection(url, username, password);
+                    System.out.println("Koneksi Database Sukses");
+                } catch (SQLException se) {
+                    System.out.println("Koneksi Database Gagal" + se);
+                    System.exit(0);
+                }
+            } catch (ClassNotFoundException cnfe) {
+                System.out.println("Class Driver tidak ditemukan, terjadi kesalahan pada : " + cnfe);
+            }
+
+        }
+        return connect;
+    }
+}
